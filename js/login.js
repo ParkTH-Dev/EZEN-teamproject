85d9fd636eeb47b8da4fd761ec39f4aca8e7357d
kim5537 <fudgml778@naver.com>
2024-08-16 17:42:22 +0900

    loginOk

--- js/login.js
+++ js/login.js
@@ -1,20 +1,22 @@
+//==document declare ===
 // modal
 const long = document.querySelector("#longModal");
-
+//form
 const form = document.querySelector("form");
-
-//user input
+//user input d
 const userId = form.querySelector("#login_id");
 const userPass = form.querySelector("#login_password");
 //random
 const autoN = form.querySelector("#autoN");
+const randomNum = Math.floor(Math.random() * 3000 + 1);
+const autoActive = document.querySelector(".automatic");
 //for submit
 const loginBtn = form.querySelector(".login");
 //modal
 const modalBack = document.querySelector("#blackB");
 const modal = document.querySelector("#longModal");
 
-//id value only EN & num
+//==id value only EN & num==
 userId.addEventListener("input", function (text) {
   const idText = userId.value;
   const onlyEnNum = /[^a-zA-Z0-9]/g;
@@ -26,11 +28,10 @@ userId.addEventListener("input", function (text) {
   }
 });
 
-// id & password submit
-const id = "ezen123";
+const id = "test";
 const password = "1234";
 
-//modal add & close
+//==modal close==
 function modalClose() {
   document.querySelectorAll(".close, #blackB").forEach((item) => {
     item.addEventListener("click", () => {
@@ -40,37 +41,43 @@ function modalClose() {
   });
 }
 
-//auto Num & auto modal Check
-function autoModal() {
-  const autoNum = Math.floor(Math.random() * 3000 + 1);
-  document.querySelector(".autonum").innerText = autoNum;
-  document.querySelector(".automatic").classList.add("active");
-  //숫자를 넣고 값을 띄우며 모달 실행
-  if (
-    autoNum === autoN.value &&
-    userId.value === id &&
-    userPass.value === password
-  ) {
+// //==auto Num input & check==
+// function autoModal() {
+//   document.querySelector(".autonum").innerText = randomNum;
+//   autoActive.classList.add("active");
+//   //auto num & id,password check
+//   autoCheck();
+// }
+
+// const auto num check
+function autoCheck() {
+  if (randomNum == autoN.value) {
     modal.querySelector("h1").innerText = `${id}님 환영합니다`;
     modalBack.classList.add("active");
     modal.classList.add("active");
     modalClose();
   } else {
-    modal.querySelector(
-      "h1"
-    ).innerHTML = `아이디,비밀번호와 <br/> 자동방지 숫자를 확인해주세요`;
+    modal.querySelector("h1").innerHTML = `자동방지 숫자를 확인해주세요`;
+    document.querySelector(".autonum").innerText = randomNum;
+    modalBack.classList.add("active");
+    modal.classList.add("active");
   }
 }
 
+// ==id,password submit & tree long count==
 let falseCount = 0;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (userId.value === id && userPass.value === password) {
-    modal.querySelector("h1").innerText = `${id}님 환영합니다`;
-    modalBack.classList.add("active");
-    modal.classList.add("active");
-    modalClose();
+    if (autoActive.classList.contains("active")) {
+      autoCheck();
+    } else {
+      modal.querySelector("h1").innerText = `${id}님 환영합니다`;
+      modalBack.classList.add("active");
+      modal.classList.add("active");
+      modalClose();
+    }
   } else {
     falseCount++;
     modalBack.classList.add("active");
@@ -78,7 +85,8 @@ form.addEventListener("submit", (e) => {
     modal.querySelector("h1").innerText = `아이디와 비밀번호를 확인해주세요`;
     modalClose();
     if (falseCount >= 3) {
-      autoModal();
+      document.querySelector(".autonum").innerText = randomNum;
+      autoActive.classList.add("active");
     }
   }
 });
