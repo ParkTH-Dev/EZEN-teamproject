c44a360f8980ebf734377a9693ea5cd8028643c5
kim5537 <fudgml778@naver.com>
2024-08-19 05:20:21 +0900

    join-ing

--- js/join.js
+++ js/join.js
@@ -1,2 +1,10 @@
 //모달 ==><div class="modalBase noneActive">에 noneActive삭제하면 나옴
 //모달은 각각 tap1Modal1 의 네임으로 되어있고 frame은 yblock 가상 클래스가 존재하여  모달 1번때만 frame에 부여하면 된다.
+
+const checkBoxs = document.querySelectorAll("input[type='checkbox']");
+
+checkBoxs.forEach((checkBox) => {
+  checkBox.addEventListener("click", () => {
+    checkBox.nextElementSibling.classList.toggle("active");
+  });
+});
