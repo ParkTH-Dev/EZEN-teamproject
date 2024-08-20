e6db13ad1afba54ea69511890e6bd289aa59e408
kim5537 <fudgml778@naver.com>
2024-08-20 19:53:23 +0900

    checkList-ing

--- js/join.js
+++ js/join.js
@@ -1,11 +1,43 @@
-//모달 ==><div class="modalBase noneActive">에 noneActive삭제하면 나옴
-//모달은 각각 tap1Modal1 의 네임으로 되어있고 frame은 yblock 가상 클래스가 존재하여  모달 1번때만 frame에 부여하면 된다.
 
+//모든체크박스들
 const checkBoxs = document.querySelectorAll("input[type='checkbox']");
+//전체동의 체크박스
+const allCheck = document.querySelector(".allCheckBox")
+// 부수요소 체크박스들
+const checkElements = document.querySelectorAll(".checkElement")
 
+// 체크박스와 아이콘 연동 함수
 checkBoxs.forEach((checkBox) => {
   checkBox.addEventListener("change", () => {
     let checkIcon = document.getElementsByClassName(`${checkBox.id}`)[0];
     checkIcon.classList.toggle("active");
   });
 });
+
+
+// 전체체크 1 // 
+
+//전체 체크를 누를 시 부수체크박스들이 전부 체크 선택
+// function all_check () {
+//   const allCheckOn = allCheck.checked
+//   if(allCheckOn === true ) {
+//     checkElements.forEach((checkElement)=>{
+//       if(checkElement.disabled !== true) {
+//         //disabled ==> 지원 속성에 속성이 적용되어있는 것
+//         checkElement.setAttribute('disabled' , 'disabled')
+//         checkElement.checked = true;
+//       }
+//     })
+//   }
+// }
+
+
+
+// 핸들러 
+
+function checkHandler (allCk , ckList) {
+let allCk = allCheck
+let ckList = checkElements
+}
+
+checkHandler(allCheck,checkElements)
