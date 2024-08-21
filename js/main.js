fb173c539171fb407b63de1591b2378ba643d7d5
Sheep-heee <sheephi0609@gmail.com>
2024-08-21 18:21:37 +0900

    project

--- js/main.js
+++ js/main.js
@@ -247,5 +247,76 @@ mainSlideArrowRight.addEventListener("mouseleave", () => {
   autoSlide();
 });
 
-// 카테고리 위치 이동
-const categoryArea = document.querySelector("#category_area");
+// 카테고리 위치 이동(MO)
+if (window.innerWidth <= 1050) {
+  const categoryArea = document.querySelector("#category_area");
+  const categoryItems = document.querySelector(".category_items");
+
+  // Touch Event
+  const listClientWidth = categoryItems.clientWidth;
+  const listScollWidth = categoryItems.clientWidth + 340;
+
+  // 최초 터치 및 마우스다운 지점
+  let startX = 0;
+
+  // 현재 이동중인 지점
+  let nowX = 0;
+
+  // 터치 종료 지점
+  let endX = 0;
+
+  // 두번째 터치 지점
+  let listX = 0;
+
+  const getClientX = (e) => {
+    return e.touches ? e.touches[0].clientX : e.clientX;
+  };
+
+  const getTranslateX = () => {
+    return parseInt(
+      getComputedStyle(categoryItems).transform.split(/[^\-0-9]+/g)[5]
+    );
+  };
+
+  const setTranslateX = (x) => {
+    categoryItems.style.transform = `translateX(${x}px)`;
+  };
+
+  const onScrollMove = (e) => {
+    nowX = getClientX(e);
+    setTranslateX(listX + nowX - startX);
+  };
+
+  const onScrollEnd = (e) => {
+    endX = getClientX(e);
+    listX = getTranslateX();
+    if (listX > 0) {
+      setTranslateX(0);
+      categoryItems.style.transition = `all 0.1s ease`;
+      listX = 0;
+    } else if (listX < listClientWidth - listScollWidth) {
+      setTranslateX(listClientWidth - listScollWidth);
+      categoryItems.style.transition = `all 0.1s ease`;
+      listX = listClientWidth - listScollWidth;
+    }
+
+    window.removeEventListener("touchstart", onScrollStart);
+    window.removeEventListener("mousedown", onScrollStart);
+    window.removeEventListener("touchmove", onScrollMove);
+    window.removeEventListener("mousemove", onScrollMove);
+    window.removeEventListener("touchend", onScrollEnd);
+    window.removeEventListener("mouseup", onScrollEnd);
+  };
+
+  const onScrollStart = (e) => {
+    startX = getClientX(e);
+
+    window.addEventListener("touchmove", onScrollMove);
+    window.addEventListener("mousemove", onScrollMove);
+    window.addEventListener("touchend", onScrollEnd);
+    window.addEventListener("mouseup", onScrollEnd);
+  };
+
+  categoryArea.addEventListener("touchstart", onScrollStart);
+  categoryArea.addEventListener("mousedown", onScrollStart);
+}
