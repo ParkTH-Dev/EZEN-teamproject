78c87deba28bcea4d3430880dfe1218c33f2c995
kim5537 <fudgml778@naver.com>
2024-08-23 19:57:17 +0900

    radioToggle-ing

--- js/join.js
+++ js/join.js
@@ -1,12 +1,120 @@
-//--------간단한 유효성 검사들 --------//
-const userId = document.querySelector("#userId")
-// const userId = document.querySelector("#userId")
-// const userId = document.querySelector("#userId")
 
+// form 요소들
+const form = document.querySelector("form")
+const inputText = document.querySelectorAll("input[type='text']")
+console.log(inputText)
+const inputpass = document.querySelectorAll("input[type='password']")
+const inputradio = document.querySelectorAll("input[type='radio']")
+const checkBoxs = document.querySelectorAll("input[type='checkbox']");
+//-----------top -----------------//
+
+////-------------각요소 조건
+
+inputText[0].addEventListener("change", function () {
+  const idErr = /^[a-zA-Z0-9]{6,16}$/g;
+  const idIn = document.querySelector(".iderr")
+  if (inputText[0].value === "") {
+    idIn.innerText = "";
+  } else if (!idErr.test(inputText[0].value)) {
+    idIn.innerText = "6자 이상 16자 이하의 영문과 숫자를 조합만 가능합니다.";
+  } else {
+    idIn.innerText = "";
+  }
+});
+
+//----pass
+inputpass[0].addEventListener("change", function () {
+  const pwErr = /^[a-zA-Z0-9`~!@#$%^&*()-_=+]{10,}$/g;
+  const passIn = document.querySelector(".passerr")
+  if (inputpass[0].value === "") {
+    passIn.innerText = "";
+  } else if (!pwErr.test(inputpass[0].value)) {
+    passIn.innerText =
+      "10자 이상에 영문 숫자 특수문자 조합만 가능합니다.";
+  } else {
+    passIn.innerText = "";
+  }
+});
+
+//----pass2
+inputpass[1].addEventListener("change", function () {
+  const pass2In = document.querySelector(".pw2err")
+  if (inputpass[1].value === "") {
+    pass2In.innerText = "";
+  } else if (inputpass[1].value === userpw.value) {
+    pass2In.innerText =
+      "";
+  } else {
+    pass2In.innerText = "동일한 비밀번호 입력해주세요.";
+  }
+});
+
+//----name
+inputText[1].addEventListener("change", function () {
+  const nameIn = document.querySelector(".nameErr")
+  const nameErr = /^[가-힣a-zA-Z]+$/g;
+  if (inputText[1].value === "") {
+    nameIn.innerText = "";
+  } else if (!nameErr.test(inputText[1].value)) {
+    nameIn.innerText =
+      "이름을 확인해주세요.";
+  } else {
+    nameIn.innerText = "";
+  }
+});
+
+//----email
+inputText[2].addEventListener("change", function () {
+  const emailIn = document.querySelector(".emailerr")
+  const emailErr =  /^[a-zA-Z0-9]$/g;
+  if (inputText[2].value === "") {
+    emailIn.innerText = "";
+  } else if (!emailErr.test(inputText[2].value)) {
+    emailIn.innerText =
+      "숫자 또는 영문만 입력 가능합니다.";
+  } else {
+    emailIn.innerText = "";
+  }
+});
+
+//----Phon
+inputText[4].addEventListener("change", function () {
+  const phonIn = document.querySelector(".phonerr")
+  const PhonV = inputText[4].value
+  const trimPhon = PhonV.replace(/-/g,"")
+  if (inputText[4].value === "") {
+    phonIn.innerText = "";
+  } else if (!/^[0][0-9]{9,10}$/.test(trimPhon)) {
+    phonIn.innerText =
+      "숫자 10~11자리";
+  } else {
+    phonIn.innerText = "";
+  }
+});
+
+//----adress
+
+
+
+//----gender & radio
+
+inputradio.addEventListener("click",()=>{
+  if (inputradio.checked) {
+    console.log("kk")
+    inputradio.checked === false;
+  } else {
+    inputradio.checked
+  }
+})
+
+
+
+//----birth
+
+//----invit
 
 //--------bottom checkbox --------//
-//모든체크박스들
-const checkBoxs = document.querySelectorAll("input[type='checkbox']");
+
 
 //--------bottom checkbox --------//
 
@@ -38,7 +146,7 @@ function checkAll(all, element) {
       checkAndIcon(checkElement); 
     }
   });
-}
+};
 
 //개별요소 선택시
 function checkToggle(all, element) {
@@ -53,7 +161,7 @@ function checkToggle(all, element) {
     allCheck.checked = false;
   }
   checkAndIcon(allCheck)
-}
+};
 
 // 핸들러
 function checkHandler(all, element) {
@@ -70,7 +178,7 @@ function checkHandler(all, element) {
       checkAndIcon(checkElement);
     });
   });
-}
+};
 
 checkHandler(".subAll", ".subElement");
 checkHandler(".allCheckBox", ".checkElement");
