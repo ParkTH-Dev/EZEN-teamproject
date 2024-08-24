e2cfea72604d62d1c98d664968ab52617c49ead0
kim5537 <fudgml778@naver.com>
2024-08-24 20:08:15 +0900

    adress-ing

--- js/join.js
+++ js/join.js
@@ -87,6 +87,67 @@ inputTexts[4].addEventListener("change", function () {
 
 //----adress
 
+// theme: {
+//   searchBgColor: "#5F0080",
+//   queryTextColor: "#FFFFFF",
+//   emphTextColor: "#5F0080",
+// },
+
+function sample6_execDaumPostcode() {
+  new daum.Postcode({
+    theme: {
+      searchBgColor: "#5F0080",
+      queryTextColor: "#FFFFFF",
+      emphTextColor: "#5F0080",
+    },
+    oncomplete: function (data) {
+      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+      // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+      // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+      var addr = ""; // 주소 변수
+      var extraAddr = ""; // 참고항목 변수
+
+      //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+      if (data.userSelectedType === "R") {
+        // 사용자가 도로명 주소를 선택했을 경우
+        addr = data.roadAddress;
+      } else {
+        // 사용자가 지번 주소를 선택했을 경우(J)
+        addr = data.jibunAddress;
+      }
+
+      // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+      if (data.userSelectedType === "R") {
+        // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+        // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+        if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+          extraAddr += data.bname;
+        }
+        // 건물명이 있고, 공동주택일 경우 추가한다.
+        if (data.buildingName !== "" && data.apartment === "Y") {
+          extraAddr +=
+            extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
+        }
+        // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+        if (extraAddr !== "") {
+          extraAddr = " (" + extraAddr + ")";
+        }
+        // 조합된 참고항목을 해당 필드에 넣는다.
+        document.getElementById("sample6_extraAddress").value = extraAddr;
+      } else {
+        document.getElementById("sample6_extraAddress").value = "";
+      }
+
+      // 우편번호와 주소 정보를 해당 필드에 넣는다.
+      document.getElementById("sample6_postcode").value = data.zonecode;
+      document.getElementById("sample6_address").value = addr;
+      // 커서를 상세주소 필드로 이동한다.
+      document.getElementById("sample6_detailAddress").focus();
+    },
+  }).open();
+}
+
 //----birth
 
 const changebirth1 = () => {
@@ -95,37 +156,31 @@ const changebirth1 = () => {
     document.querySelector("#moth").focus();
   }
 };
+
 const changebirth2 = () => {
   const birth2 = document.querySelector("#moth").value;
   if (birth2.length === 2) {
     document.querySelector("#day").focus();
   }
 };
+
 const changebirth3 = () => {
   const birth1 = document.querySelector("#year").value;
   const birth2 = document.querySelector("#moth").value;
   const birth3 = document.querySelector("#day").value;
   const birthIn = document.querySelector(".birtherr");
-
-  if (birth1 === "" && birth2 === "" && birth3 === "") {
+  if (birth3.length === 2) {
+    document.querySelector("#day").blur();
+  } else if (
+    /^[0-9]+$/g.test(birth1) &&
+    /^[0-9]+$/g.test(birth2) &&
+    /^[0-9]+$/g.test(birth3)
+  ) {
     birthIn.innerText = "";
-  } else if (!/^[0-9]$/g.test(trimPhon)) {
-    birthIn.innerText = "숫자만 입력해주세요";
   } else {
-    birthIn.innerText = "";
+    birthIn.innerText = "숫자만 입력해주세요.";
   }
 };
-
-// const birth1Fnt = () => {
-//   if (birth1.length === 4) {
-//     inputTexts[6].focus();
-//   }
-// };
-// const birth2Fnt = () => {
-//   if (birth2.length === 2) {
-//     inputTexts[7].focus();
-//   }
-// };
 
 //----invit
 
